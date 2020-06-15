4508ac60773d220bc83525124f9cb33fa3f0fca4
nuesslerm <markus@choco.com>
2020-06-16 00:00:51 +0200

    made navbar better 2

--- src/react/components/pic-dropdown/pic-dropdown.component.jsx
+++ src/react/components/pic-dropdown/pic-dropdown.component.jsx
@@ -3,7 +3,11 @@ import React from 'react';
 import './pic-dropdown.styles.scss';
 
 const PicDropdown = () => (
-  <div className="dropdown-menu" style={{ right: '0', left: 'auto' }}>
+  <div
+    className="dropdown-menu"
+    // this style makes the dropdown expand to the left, when screen width decreases
+    style={{ right: '0', left: 'auto' }}
+  >
     <div className="dropdown-item" onClick={() => history.push('/')}>
       Pic 1
     </div>
