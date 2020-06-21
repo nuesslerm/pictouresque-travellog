4b4e9ff71f6e0b91a03d69bba2e1b7ee345e7031
nuesslerm <markus@choco.com>
2020-06-21 23:02:12 +0200

    init 5

--- server/src/index.js
+++ server/src/index.js
@@ -1,9 +1,22 @@
 const express = require('express');
 
+// import middle-ware with require
+const morgan = require('morgan');
+const helmet = require('helmet');
+const cors = require('cors');
+
 const app = express();
+// setup middle-ware
+app.use(morgan('common'));
+app.use(helmet());
+app.use(
+  cors({
+    origin: 'http://localhost:8080',
+  })
+);
 
 const port = process.env.PORT || 4000;
 
 app.listen(port, () => {
-  console.log('Listening at http://localhost:1337');
+  console.log(`Listening at http://localhost:${port}`);
 });
