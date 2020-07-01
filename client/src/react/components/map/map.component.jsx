503fca771e6d47fd2973895ff1cde33ebcd00faf
nuesslerm <markus@choco.com>
2020-07-01 02:14:18 +0200

    create marker and popup for locations from server 1

--- client/src/react/components/map/map.component.jsx
+++ client/src/react/components/map/map.component.jsx
@@ -1,8 +1,10 @@
 import React, { useState, useEffect } from 'react';
-import ReactMapGL, { Marker } from 'react-map-gl';
+import ReactMapGL, { Marker, Popup } from 'react-map-gl';
 import MapStylePicker from '../map-style-picker/map-style-picker.component.jsx';
 
 import { listLogEntries } from '../../API.js';
+
+// import Pin from '../../../assets/map-pin.svg';
 
 import './map.styles.scss';
 
@@ -17,6 +19,7 @@ const REACT_APP_MAPBOX_TOKEN =
 
 const Map = () => {
   const [style, setStyle] = useState('mapbox://styles/mapbox/light-v9');
+  const [showPopup, setShowPopup] = useState({});
   const [viewport, setViewport] = useState({
     width: window.innerWidth,
     height: window.innerHeight,
@@ -31,7 +34,7 @@ const Map = () => {
     (async () => {
       const logEntries = await listLogEntries();
       setLogEntries(logEntries);
-      console.log(logEntries);
+      // console.log(logEntries);
     })();
 
     window.addEventListener('resize', _resize);
@@ -80,17 +83,85 @@ const Map = () => {
         onViewportChange={(viewportChange) => _onViewportChange(viewportChange)}
       >
         {logEntries.map((entry) => (
-          <Marker
-            // in map, you can use {object._id} to list a unique id,
-            // based on the objects placement in the array of objects
-            key={entry._id}
-            latitude={entry.latitude}
-            longitude={entry.longitude}
-            offsetLeft={-20}
-            offsetTop={-10}
-          >
-            <div>{entry.title}</div>
-          </Marker>
+          // because there are two html elements listed after one another,
+          // we need to wrap them in a fragment <> </> (or just a div)
+          <>
+            <Marker
+              // in map, you can use {object._id} to list a unique id,
+              // based on the objects placement in the array of objects
+              key={entry._id}
+              latitude={entry.latitude}
+              longitude={entry.longitude}
+              // offsetLeft={-12}
+              // offsetTop={-24}
+            >
+              {
+                /* #region PIN */
+                // <Pin
+                //   className="pin-container"
+                //   style={{
+                //     /**
+                //      * The calc() CSS function lets you perform calculations when
+                //      * specifying CSS property values. It can be used anywhere a
+                //      * <length>, <frequency>, <angle>, <time>, <percentage>, <number>, or <integer> is allowed.
+                //      * https://css-tricks.com/simple-little-use-case-vmin/
+                //      */
+                //     wdith: `calc(1vmin * ${viewport.zoom})`,
+                //     height: `calc(1vmin * ${viewport.zoom})`,
+                //   }}
+                // />
+                /* #endregion */
+              }
+              <div
+                onClick={() =>
+                  setShowPopup({
+                    /** only set the entry._id key - value to true, which was selected
+                     * we don't want to cache any key-value pairs
+                     */
+                    // ...showPopup,
+                    [entry._id]: true,
+                  })
+                }
+              >
+                <img
+                  className="pin-container"
+                  style={{
+                    wdith: `${5 * viewport.zoom}px`,
+                    height: `${5 * viewport.zoom}px`,
+                  }}
+                  src="https://i.imgur.com/y0G5YTX.png"
+                  alt="pin"
+                />
+              </div>
+            </Marker>
+            {showPopup[entry._id] ? (
+              <Popup
+                latitude={entry.latitude}
+                longitude={entry.longitude}
+                closeButton={true}
+                closeOnClick={true}
+                onClose={() =>
+                  setShowPopup({
+                    /** set showPopup to an *empty object*, so that all entry._id keys will
+                     * receive a null value, whenever the user closes the popup
+                     * we don't want to cache any key-value pairs
+                     * */
+                    // ...showPopup,
+                    // [entry._id]: false,
+                  })
+                }
+                anchor="top"
+              >
+                <div className="map-popup-container">
+                  {
+                    // TODO: set z-index dynamically, so that the pop-ups don't overlap
+                  }
+                  <h4>{entry.title}</h4>
+                  <p>{entry.comments}</p>
+                </div>
+              </Popup>
+            ) : null}
+          </>
         ))}
       </ReactMapGL>
     </div>
